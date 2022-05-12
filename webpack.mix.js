a14e398446fb0d33d3867b215f5a0ffdd96f3516
Edvinas Karalis <95039110+HHvis@users.noreply.github.com>
2022-05-12 18:55:22 +0300

    WebPack Error Solution

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,9 @@ mix.js('resources/js/app.js', 'public/js')
     .react()
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+
+mix.webpackConfig({
+    stats: {
+        children: true,
+    },
+});
