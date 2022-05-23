9ef0ec01f94bd04fc15c2671f4664a54dca190b6
KristinaSton <95041585+KristinaSton@users.noreply.github.com>
2022-05-23 09:35:41 +0300

    Added decimals

--- resources/js/components/IncomeTable/CreateIncomeModal.js
+++ resources/js/components/IncomeTable/CreateIncomeModal.js
@@ -57,7 +57,8 @@ class CreateIncomeModal extends Component {
           isValid = false;
         }
         else if (amount.includes('.')) {
-         if(amount.split('.')[0].length>4){
+         if(amount.split('.')[1].length>4 ||
+           amount.split('.')[0].length>2){
           errors.amountTooLong = "Sumažinkite sumą. Suma negali viršyti keturženklės sumos ir dviejų skaičių po kalbelio";
           isValid = false;
           }else {
@@ -65,8 +66,7 @@ class CreateIncomeModal extends Component {
           }   
         }
         else if (!amount.includes('.')) {
-          if(amount.trim().length>4 ||
-           amount.trim().length>2){
+          if(amount.trim().length>6){
            errors.amountTooLong = "Sumažinkite sumą. Suma negali viršyti keturženklės sumos ir dviejų skaičių po kalbelio";
            isValid = false;
           }else {
