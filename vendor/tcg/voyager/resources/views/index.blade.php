b2cbf8f25abcb258b6c1a532e82ed431186bb048
Asta <95692177+AstaGok@users.noreply.github.com>
2022-05-26 13:07:09 +0300

    Update index.blade.php

--- vendor/tcg/voyager/resources/views/index.blade.php
+++ vendor/tcg/voyager/resources/views/index.blade.php
@@ -1,6 +1,77 @@
 @extends('voyager::master')
 
 @section('content')
-<p>Įvykiai</p>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <title>VTMCphp</title>
+
+        <!-- Fonts -->
+  <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+  
+        <!-- Styles -->
+    </head>
+<body>
+<div class="container">
+  <section>
+    <div class="row">
+      <div class="col-12 mt-3 mb-1">
+        <h5 class="text-uppercase">Biudžeto svetainės administravimas</h5>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-xl-3 col-sm-4 col-12 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <div class="d-flex justify-content-between px-md-1">
+              <div class="align-self-center">
+                <i class="fas fa-pencil-alt text-info fa-3x"></i>
+              </div>
+              <div class="text-end">
+                <h3>Koreguoti</h3>
+                <a href="admin/expense_category"><p class="mb-0 font-weight-bold">Kategorijas →</p></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xl-3 col-sm-4 col-12 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <div class="d-flex justify-content-between px-md-1">
+              <div class="align-self-center">
+                <i class="far fa-user text-success fa-3x"></i>
+              </div>
+              <div class="text-end">
+                <h3>Koreguoti</h3>
+                
+                <a href="admin/users"><p class="mb-0 font-weight-bold">Vartotojus →</p></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> 
+      <div class="col-xl-3 col-sm-4 col-12 mb-4">
+        <div class="card">
+          <div class="card-body">
+            <div class="d-flex justify-content-between px-md-1">
+              <div class="align-self-center">
+                <i class="far fa-life-ring text-info fa-3x"></i>
+              </div>
+              <div class="text-end">
+                <h3>Peržiūrėti</h3>
+                <a href="home"><p class="mb-0 font-weight-bold">Asmeninio biudžeto puslapį →</p></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+  </section>
+</div>
+</body>
 @stop
